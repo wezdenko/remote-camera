34c68d362b9ec80067c176d1da24686ab1b58a3a
Mateusz Wezdeńko <mateusz.wezdenko.stud@pw.edu.pl>
2021-04-27 13:59:09 +0200

    added ObjectDetection class

---
+++ lib/ObjectDetection.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+
+class ObjectDetection {
+  private:
+    cv::Mat *frame;
+    int threshold;
+
+    std::vector<cv::Vec4i> hierarchy;
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<std::vector<cv::Point>> approxContours;
+
+  public:
+    ObjectDetection(int threshold);
+    ~ObjectDetection();
+
+    void loadFrame(cv::Mat &frame);
+    cv::Point2d detectObject(int numOfEdges);
+
+private:
+    cv::Point2d getObjectCenter(int numOfEdges);
+};
