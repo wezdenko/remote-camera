b2ebbe34c7dde261fb98873684467ba3421ca24a
mjankow3 <mateusz.jankowski6.stud@pw.edu.pl>
2021-05-04 13:26:53 +0200

    Created template class needed to transfer vectors

--- client/app/main.cpp
+++ client/app/main.cpp
@@ -2,21 +2,31 @@
 #include <arpa/inet.h>
 #include "SocketConnector.hpp"
 #include "FileSender.hpp"
+#include "VectorSender.hpp"
+#include <memory>
 
 int main()
 {
-    auto connection = SocketConnector(AF_INET, SOCK_STREAM);
-    if(!connection.connectToServer("172.17.0.1", 54000)){
-        std::cerr<<"CANT CONNECT TO SERVER!";
-        return -1;
-    }
-    std::cout<<"CONNECTED!";
-    auto sender = FileSender();
-    sender.transferFile([&](std::string data){
-        if(data.size() > 4) connection.sendData(data, 20);
-        else connection.sendData(data, 3);
-    }, "fotodziala");
-    connection.closeConnection();
-    return 0;
+    std::vector<int> vec = {5,6,7,8,9,10};
+    auto vecSender = std::make_unique<VectorSender<int>>(vec);
+    FileTransferer* dataTransferer;
+    dataTransferer = vecSender.get();
+    dataTransferer->transferFile([&](std::string data){
+        std::cout<<data<<"\n";
+    }, "TEST");
+
+    // auto connection = SocketConnector(AF_INET, SOCK_STREAM);
+    // if(!connection.connectToServer("172.17.0.1", 54000)){
+    //     std::cerr<<"CANT CONNECT TO SERVER!";
+    //     return -1;
+    // }
+    // std::cout<<"CONNECTED!";
+    // auto sender = FileSender();
+    // sender.transferFile([&](std::string data){
+    //     if(data.size() > 4) connection.sendData(data, 20);
+    //     else connection.sendData(data, 3);
+    // }, "fotodziala");
+    // connection.closeConnection();
+    // return 0;
 
 }
---
+++ client/lib/VectorSender.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include "FileTransferer.hpp"
+
+template<typename T>
+class VectorSender: public FileTransferer{
+    public:
+        VectorSender(const std::vector<T>& dataVec): data(dataVec){};
+        virtual void transferFile(std::function<void(std::string)> transfer, std::string fileName){
+            transfer(fileName);
+            for(const auto& singleData : data){
+                transfer(std::to_string(singleData));
+            }
+        }
+        void setVector(const std::vector<T>& data){
+            this->data = data;
+        }
+    private:
+        std::vector<T> data;
+};
